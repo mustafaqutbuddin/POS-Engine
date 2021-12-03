295b909043ee057c9aa35f5c45874c53172761be
Mustafa <mustafa@voga>
2021-12-03 09:36:24 -0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+
+import PackageDescription
+
+let package = Package(
+  name: "CalendarControl",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v14), .tvOS(.v14)
+  ],
+  products: [
+    .library(
+      name: "PosEngine",
+      targets: ["PosEngine"]),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "PosEngine",
+      path: "./Sources/PosEngine.xcframework")
+  ]
+)
---
+++ Sources/PosEngine.xcframework/ios-arm64/PosEngine.framework/Headers/POS.h
@@ -0,0 +1,18 @@
+//
+//  POS.h
+//  POS
+//
+//  Created by Mustafa on 2021-11-29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POS.
+FOUNDATION_EXPORT double POSVersionNumber;
+
+//! Project version string for POS.
+FOUNDATION_EXPORT const unsigned char POSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POS/PublicHeader.h>
+
+
